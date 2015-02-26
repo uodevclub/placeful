a9365a7ca763978b2ee39011abf62796977627a7
Jeff Bayes <jbayes92@gmail.com>
2015-02-25 20:11:41 -0800

    Woohoo!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # placeful
 Placeful is an anonymized, location-based message platform for your world to enjoy.
+
+
+### Installing
+    pip install -r requirements.txt
--- placeful.py
+++ placeful.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+#### VIEWS.PY ####
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+###################
+#### MODELS.PY ####
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
+
+###################
 
 if __name__ == '__main__':
     app.run(debug = True)
