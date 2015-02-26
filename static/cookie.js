bb61d22a18fda718ff9650e459a1480808fbd2dc
Nathan Pointer <nathanpointer@128-223-223-67-wireless.uoregon.edu>
2015-02-26 09:25:06 -0800

    added a cookie bases cashe for location info so app is not so slow

---
+++ static/cookie.js
@@ -0,0 +1,21 @@
+setCookie = function(cname, cvalue, hours) {
+    var d = new Date();
+    d.setTime(d.getTime() + (hours*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+getCookie = function(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function deleteCookie( name ) {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
