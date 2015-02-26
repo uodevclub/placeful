5b71fd0abff4f813428b130c466fba28b7365c3c
Nathan Pointer <nathanpointer@128-223-223-66-wireless.uoregon.edu>
2015-02-25 20:37:27 -0800

    post route saving data

--- placeful.py
+++ placeful.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def hello():
+    message = request.form['message']
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+    # save it to db
+    return render_template('index.html')
+
+# Run the app :)
+
 if __name__ == '__main__':
     app.run(debug = True)
+
